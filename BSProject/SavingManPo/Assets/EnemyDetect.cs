b355c92ec36d492cadfca83813ac971db3b85d26
jx <823421641@qq.com>
2016-03-20 16:50:41 +0800

    3.20 第一关版本

---
+++ BSProject/SavingManPo/Assets/EnemyDetect.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDetect : MonoBehaviour {
+    private Animator animator;
+    public bool flip = false;
+    private bool die = false;
+    public GameObject angry,why;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(animator)
+        {
+            if(animator.GetCurrentAnimatorStateInfo(0).IsName("Walk")||
+                animator.GetCurrentAnimatorStateInfo(0).IsName("lookAround"))
+            {
+                int direction = flip ? 1 : -1;
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right*direction,10, (1 <<9|1<<10));
+                Debug.DrawLine(transform.position, transform.right * 100*direction + transform.position, Color.red);
+                if (hit.collider!=null)
+                {
+                    if (!die&& hit.collider.gameObject.name == "manpo")
+                    {
+                        die = true;
+                        Die();
+                    }
+                }
+            }
+        }
+	}
+
+    void Die()
+    {
+        animator.enabled = false;
+        iTween.Stop(gameObject);
+        GameObject.Find("manpo").GetComponent<CharacterController>().StopAllCoroutines();
+        angry.SetActive(true);
+        StartCoroutine(ReloadLevel());
+    }
+
+    IEnumerator ReloadLevel()
+    {
+        yield return new WaitForSeconds(1);
+        GameObject.Find("FadeOut").GetComponent<iTweenAlphaTo>().iTweenPlay();
+        yield return new WaitForSeconds(2);
+        Application.LoadLevel(0);
+    }
+}
